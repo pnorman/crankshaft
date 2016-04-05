4c4a62469fd437c918e29ce3dbeb51a3a455305a
Andy Eschbacher <andy.eschbacher@gmail.com>
2016-04-05 16:20:28 -0400

    adding exceptions, etc.

--- src/pg/sql/45_observatory_geometries.sql
+++ src/pg/sql/45_observatory_geometries.sql
@@ -1,38 +1,55 @@
-
 -- Returns the polygon(s) that overlap with the input geometry.
 -- Input:
 -- :param geom geometry: input geometry
 -- :param geometry_level text: table to get polygon from (can be approximate name)
 -- :param use_literal boolean: use the literal table name (defaults to true)
 
--- From an input geometry (e.g., point), find the boundary roughly requested
+-- From an input point geometry, find the boundary which intersects with the centroid of the input geometry
 
 CREATE OR REPLACE FUNCTION OBS_Get_Geometry(
-  geom geometry,
+  geom geometry(Geometry, 4326),
   geometry_level text DEFAULT 'observatory.obs_a6b7e2e5de1ba72555fa54c8bf3ecc717d6161b4',
-  use_literal boolean DEFAULT true,
-  time_span text DEFAULT '2009 - 2013'
-)
-RETURNS geometry
+  use_literal boolean DEFAULT false,
+  time_span text DEFAULT '2009 - 2013')
+  RETURNS geometry(Geometry, 4326)
 AS $$
 DECLARE
-  boundary geometry;
+  boundary geometry(Geometry, 4326);
   target_table text;
+  target_table_list text[];
 BEGIN
+
+  -- TODO: Check if SRID = 4326, if not transform?
+
+  -- if not a point, raise error
+  IF ST_GeometryType(geom) != 'ST_Point'
+  THEN
+    RAISE EXCEPTION 'Invalid geometry type (%), expecting ''ST_Point''', ST_GeometryType(geom);
+  END IF;
 
   IF use_literal
   THEN
     target_table := geometry_level;
   ELSE
-    target_table := OBS_Search_Tables(geometry_level::text);
+    target_table_list := OBS_Search_Tables(geometry_level, time_span);
+
+    -- if no tables are found, raise error
+    IF array_length(target_table_list, 1) IS NULL
+    THEN
+      RAISE EXCEPTION 'No boundaries found for ''%''', geometry_level;
+    ELSE
+    -- else, choose first result
+      target_table = target_table_list[1];
+    END IF;
+
     RAISE NOTICE 'target_table: %', target_table;
   END IF;
 
   EXECUTE format(
     'SELECT t.the_geom
-    FROM observatory.%s As t
-    WHERE ST_Intersects($1, t.the_geom)
-    LIMIT 1', target_table)
+     FROM observatory.%s As t
+     WHERE ST_Intersects($1, t.the_geom)
+     LIMIT 1', target_table)
   INTO boundary
   USING geom;
 
@@ -46,26 +63,38 @@ $$ LANGUAGE plpgsql;
 CREATE OR REPLACE FUNCTION OBS_Get_Geometry(
   address text,
   geometry_level text DEFAULT 'observatory.obs_a6b7e2e5de1ba72555fa54c8bf3ecc717d6161b4',
-  use_literal boolean DEFAULT true,
+  use_literal boolean DEFAULT false,
   time_span text DEFAULT '2009 - 2013'
 )
-RETURNS geometry
+RETURNS geometry(Geometry, 4326)
 AS $$
 DECLARE
-  boundary geometry;
-  address_geom geometry;
+  boundary geometry(Geometry, 4326);
+  address_geom geometry(Geometry, 4326);
   target_table text;
+  target_table_list text[];
 BEGIN
 
   IF use_literal
   THEN
     target_table := geometry_level;
   ELSE
-    target_table := OBS_Search_Tables(geometry_level::text);
+    target_table_list := OBS_Search_Tables(geometry_level, time_span);
+
+    -- if no tables are found, raise error
+    IF array_length(target_table_list, 1) IS NULL
+    THEN
+      RAISE EXCEPTION 'No boundaries found for ''%''', geometry_level;
+    ELSE
+    -- else, choose first result
+      target_table = target_table_list[1];
+    END IF;
+
     RAISE NOTICE 'target_table: %', target_table;
   END IF;
 
-  address_geom = cdb_geocode_street_point(address);
+  -- get lat/long for street-level address
+  address_geom := cdb_geocode_street_point(address);
 
   EXECUTE format(
     'SELECT t.the_geom
@@ -76,6 +105,113 @@ BEGIN
   USING address_geom;
 
   RETURN boundary;
+
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION OBS_Get_Geometry_Id(
+  geom geometry(Geometry, 4326),
+  geometry_level text DEFAULT 'observatory.obs_a6b7e2e5de1ba72555fa54c8bf3ecc717d6161b4',
+  use_literal boolean DEFAULT false,
+  time_span text DEFAULT '2009 - 2013'
+)
+RETURNS text
+AS $$
+DECLARE
+  output_id text;
+  target_table text;
+  target_table_list text[];
+BEGIN
+
+  -- If not point, raise error
+  IF ST_GeometryType(geom) != 'ST_Point'
+  THEN
+    RAISE EXCEPTION 'Error: Invalid geometry type (%), expecting ''ST_Point''', ST_GeometryType(geom);
+  END IF;
+
+  -- find table
+  IF use_literal
+  THEN
+    target_table := geometry_level;
+  ELSE
+    target_table_list := OBS_Search_Tables(geometry_level, time_span);
+
+    -- if no tables are found, raise error
+    IF target_table_list = '{}'
+    THEN
+      RAISE EXCEPTION 'Error: No boundaries found for ''%''', geometry_level;
+    ELSE
+      target_table = target_table_list[1];
+    END IF;
+
+    RAISE NOTICE 'target_table: %', target_table;
+  END IF;
+
+  -- return name of geometry id column
+
+  EXECUTE format(
+    'SELECT t.geoid
+    FROM observatory.%s As t
+    WHERE ST_Intersects($1, t.the_geom)
+    LIMIT 1', target_table)
+  INTO output_id
+  USING geom;
+
+  RETURN output_id;
+
+END;
+$$ LANGUAGE plpgsql;
+
+
+CREATE OR REPLACE FUNCTION OBS_Get_Geometry_Id(
+  address text,
+  geometry_level text DEFAULT 'observatory.obs_a6b7e2e5de1ba72555fa54c8bf3ecc717d6161b4',
+  use_literal boolean DEFAULT false,
+  time_span text DEFAULT '2009 - 2013'
+)
+RETURNS text
+AS $$
+DECLARE
+  output_id text;
+  address_geom geometry(Geometry, 4326);
+  target_table text;
+  target_table_list text[];
+BEGIN
+
+  IF use_literal
+  THEN
+    -- use input table name
+    target_table := geometry_level;
+  ELSE
+    -- find an appropriate table
+    target_table_list := OBS_Search_Tables(geometry_level, time_span);
+
+    -- if no tables are found, raise error
+    IF target_table_list = '{}'
+    THEN
+      RAISE EXCEPTION 'Error: No boundaries found for ''%''', geometry_level;
+    ELSE
+      target_table = target_table_list[1];
+    END IF;
+
+    RAISE NOTICE 'target_table: %', target_table;
+  END IF;
+
+  -- find location of address
+  address_geom := cdb_geocode_street_point(address);
+
+  -- find geoid of polygon in target_table
+  --  which intersects with this address
+  EXECUTE format(
+    'SELECT t.geoid
+    FROM observatory.%s As t
+    WHERE ST_Intersects($1, t.the_geom)
+    LIMIT 1', target_table)
+  INTO output_id
+  USING address_geom;
+  RAISE NOTICE 'geoid: %', output_id;
+
+  RETURN output_id;
 
 END;
 $$ LANGUAGE plpgsql;
